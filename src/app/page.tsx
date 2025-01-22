d19f3d56dcd854bd9444b3e69631609fa0b00a74
Maagztech <pasutosh15@gmail.com>
2025-01-22 09:07:29 +0530

    Enhance Home page layout with login and registration buttons, add game description, and update styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,79 @@ import OnboardMobileBg from "@/assets/onboardMobileBg.svg";
 import { useGlobalContext } from "@/context/globalContext";
 import { GoogleLogin } from "@react-oauth/google";
 import GlobalLoader from "@/components/GlobalLoader";
+import Link from "next/link";
+import poster from "@/assets/damangameposter.webp"
 
 export default function Home() {
   const { isMobile, isLoading, signInUser }: any =
     useGlobalContext();
   if (isMobile === null) return <></>;
   return (
-    <main className="h-[100vh] w-full overflow-hidden">
+    <main className="w-full overflow-hidden px-[20px] sm:px-[30px] md:px-[100px]" style={{ fontFamily: "Overpass, sans-serif" }}>
       {isLoading ? <GlobalLoader /> : <></>}
-      {isMobile ? (
+      <div className="flex items-center justify-center gap-4 mt-3">
+        <div className="relative h-[42px] w-[311px] mt-[28px] mb-[35px] flex justify-center">
+          <div
+            style={{ zIndex: 3 }}
+            className="opacity-0 absolute top-0 right-0 cursor-pointer hover:scale-105 active:scale-95 transition-transform duration-150 ease-in-out "
+          >
+            <GoogleLogin
+              width="400px"
+              onSuccess={(credentialResponse) => {
+                signInUser(credentialResponse.credential);
+              }}
+              onError={() => {
+                console.log("Login Failed");
+              }}
+            />
+          </div>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+            }}
+            style={{ zIndex: 1 }}
+            className="opacity-100 rounded-[8px] flex flex-row justify-center items-center h-[42px] absolute top-[3px] py-2 px-4 bg-red-600 text-white fony-bold rouded-md"
+          >
+            Login
+          </button>
+        </div>
+        <div className="relative h-[42px] w-[311px] mt-[28px] mb-[35px] flex justify-center">
+          <div
+            style={{ zIndex: 3 }}
+            className="opacity-0 absolute top-0 right-0 cursor-pointer hover:scale-105 active:scale-95 transition-transform duration-150 ease-in-out "
+          >
+            <GoogleLogin
+              width="400px"
+              onSuccess={(credentialResponse) => {
+                signInUser(credentialResponse.credential);
+              }}
+              onError={() => {
+                console.log("Login Failed");
+              }}
+            />
+          </div>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+            }}
+            style={{ zIndex: 1 }}
+            className="opacity-100 rounded-[8px] flex flex-row justify-center items-center h-[42px] absolute top-[3px] py-2 px-4 bg-red-600 text-white fony-bold rouded-md"
+          >
+            Register
+          </button>
+        </div>
+      </div>
+      <div className="flex items-center justify-between mt-3">
+        <h1 className="text-black font-bold text-[28px] sm:text-[36px]">Dhaman Game Login</h1>
+        <Link href="/"><p className="text-[blue] hover:underline">Home</p></Link>
+      </div>
+
+      <hr />
+      <p className="mt-[80px] text-[25px]">Login to play game.</p>
+      <div className="flex justify-center">
+        <img src={poster.src} alt="" className="w-[300px] mt-[80px]" />
+      </div>
+      {/* {isMobile ? (
         <div className="w-full h-full flex flex-col fixed">
           <div className="flex-grow">
             <img src={OnboardMobile.src} alt="" className="w-full mt-[100px]" />
@@ -29,33 +93,9 @@ export default function Home() {
               style={{ fontFamily: "Overpass, sans-serif" }}
               className="font-medium text-[32px] leading-[38.4px] z-20 mt-[62.65px]"
             >
-              Win Go
+              Dhaman
             </p>
-            <div className="relative h-[42px] w-[311px] mt-[28px] mb-[35px] flex justify-center">
-              <div
-                style={{ zIndex: 3 }}
-                className="opacity-0 absolute top-0 right-0 cursor-pointer hover:scale-105 active:scale-95 transition-transform duration-150 ease-in-out "
-              >
-                <GoogleLogin
-                  width="400px"
-                  onSuccess={(credentialResponse) => {
-                    signInUser(credentialResponse.credential);
-                  }}
-                  onError={() => {
-                    console.log("Login Failed");
-                  }}
-                />
-              </div>
-              <button
-                onClick={(e) => {
-                  e.preventDefault();
-                }}
-                style={{ zIndex: 1 }}
-                className="opacity-100 rounded-[8px] flex flex-row justify-center items-center h-[42px] absolute top-[3px]"
-              >
-                <img src={EnterButton.src} alt="" />
-              </button>
-            </div>
+            
           </div>
         </div>
       ) : (
@@ -101,8 +141,15 @@ export default function Home() {
             </button>
           </div>
         </div>
-      )}
+      )} */}
+      <p className="mt-[60px]">If you often play the colour prediction game, you might need a legit and fair gaming app to play the colour prediction game. That’s why the most obvious choice to play the colour prediction game would be the Daman Game app. </p>
 
+      <p className="mt-[60px]">Customer Support – <Link href="https://t.me/VIPTeacherAryahh" className="text-blue-500">https://t.me/VIPTeacherAryahh</Link></p>
+
+      <p className="mt-[60px]">The Daman Game is the best colour prediction app where you can play the colour prediction game in 1 minute, 3 minutes, 5 minutes and 10 minutes. The Daman Game app also gives a win streak and rebate bonus for their colour prediction game.</p>
+      <div className="bg-gray-200 p-4 py-[60px] mt-[70px]">
+        <p>Copyright @2025 Dhaman Game Login</p>
+      </div>
     </main>
   );
 }
